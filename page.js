34608c1b0de9da2754ab58e598d2ddd72245feb1
Sai Prajoth <87938462+SaiPrajoth@users.noreply.github.com>
2024-02-09 23:06:29 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-900 w-screen h-screen p-4 flex ">
+      
+      <button className=" bg-white text-black m-auto p-4 rounded-lg">Login with Google</button>
+
+    </div>
+  );
+}
